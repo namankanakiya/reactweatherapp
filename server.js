8458b6c5b118e735b719698285f644d228e7af13
Naman Kanakiya <naman.kanakiya@gmail.com>
2017-01-10 20:54:15 -0500

    updated code for Heroku use

--- server.js
+++ server.js
@@ -2,9 +2,19 @@ var express = require('express');
 
 // Create the app
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function (req, res, next) {
+	if (req.headers['x-forwarded-proto'] === 'http') {
+		next();
+	} else {
+		res.redirect('http://' + req.hostname + req.url);
+	}
+});
+
 app.use(express.static('public'));
 
 //start the server
-app.listen(3000, function () {
-	console.log('Express server is up on port 3k');
+app.listen(PORT, function () {
+	console.log('Express server is up on port ' + PORT);
 });
